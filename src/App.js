e7e6cf4ad093a65a87c657155c57fe67c526fe05
imrn16 <151978120+imrn16@users.noreply.github.com>
2024-09-30 23:09:40 -0500

    modified:   src/App.js 	new file:   src/TodoItem.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todo, setTodo] = useState([]);
+	const [newName, setNewName] = useState("");
+
+	function addButton() {
+		if (newName === "") {return}
+		console.log("added");
+		setTodo((current) => {
+			return [...current, { id: crypto.randomUUID(), checky: false, name: newName }];
+		});
+		setNewName("");
+		console.log({ todo });
+	}
+
+	function checkComplete(ident, checks) {
+		setTodo((cur) => {
+			return cur.map((ind) => {
+				if (ind.id === ident) {
+					return { ...ind, checky: checks };
+				}
+				return ind;
+			});
+		});
+	}
+
+	function deleteTask(ide) {
+		setTodo((curr) => {
+			return curr.filter((indiv) => indiv.id !== ide);
+		});
+	}
+
+	return (
+		<>
+			<h2>To Do</h2>
+			<input
+				type=""
+				placeholder="New Item"
+				value={newName}
+				onChange={(e) => setNewName(e.target.value)}></input>
+			<button onClick={() => addButton()}>Add</button>
+			<h3>List</h3>
+			<ul>
+				{todo.map((indTodo) => {
+					return (
+						<li key={indTodo.id}>
+							<input
+								type="checkbox"
+								checked={indTodo.checky}
+								onChange={(e) => checkComplete(indTodo.id, e.target.checked)}></input>
+							<span>{indTodo.name}</span>
+							<button onClick={() => deleteTask(indTodo.id)}>Delete</button>
+						</li>
+					);
+				})}
+			</ul>
+		</>
+	);
 }
 
 export default App;
